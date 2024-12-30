037f444178f4cdf27291b4cc851529325f86873b
Siddhartha404 <sthapa-chhetri2@myseneca.ca>
2024-12-30 02:02:13 -0700

    started using sprite classes

--- Space-Invaders/main.py
+++ Space-Invaders/main.py
@@ -1,53 +1,88 @@
 import pygame
 import pygame.display
 from random import randint
+
+
 #initializing
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 running = True
+clock = pygame.time.Clock()
 
-#surface
 
 #saceship
-spaceship = pygame.image.load('images/spaceship.png').convert_alpha()
-spaceship_rect = spaceship.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT-50))
-direction = 1
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self, groups, img):
+        super().__init__(groups)
+        self.image = img
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT-50))
+        self.direction = pygame.math.Vector2(1,1)
+        self.speed = 300
+    def update(self, dt):
+        #spaceship movement
+        key = pygame.key.get_pressed()
+        self.direction.x = int(key[pygame.K_RIGHT]) - int(key[pygame.K_LEFT])
+        self.direction.y = int(key[pygame.K_DOWN]) - int(key[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
 
-#Meteor
-meteor = pygame.image.load('images/meteor.png').convert_alpha()
-meteor_rect = meteor.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, img):
+        super().__init__(groups)   
+        self.image = img
+        self.rect = self.image.get_frect(center=((randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT))))
+    
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, groups, img):
+        super().__init__(groups)   
+        self.image = img
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
-#laser
-laser = pygame.image.load('images/laser.png').convert_alpha()
-laser_rect = meteor.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, groups, img):
+        super().__init__(groups)   
+        self.image = img
+        self.rect = self.image.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
 
-#all the stars
-star = pygame.image.load('images/star.png').convert_alpha()
-positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
+#all imports
+spaceship_img = pygame.image.load('images/spaceship.png').convert_alpha()
+star_img = pygame.image.load('images/star.png').convert_alpha()
+metero_img = pygame.image.load('images/meteor.png').convert_alpha()
+laser_img = pygame.image.load('images/laser.png').convert_alpha()
+
+all_sprite = pygame.sprite.Group()
+for i in range(20):
+    Star(all_sprite, star_img)
+meteor = Meteor(all_sprite, metero_img)
+laser = Laser(all_sprite, laser_img)
+spaceship = Spaceship(all_sprite, spaceship_img)
+
+
+
+
+
+
 #Main loop
 while running:
+    dt = clock.tick() / 1000
     #event loop
     for event in pygame.event.get():
         #quitting the game
         if event.type == pygame.QUIT:
             running = False
+    
+    all_sprite.update(dt)
+
+    #drawing the screen
     screen.fill("darkgray")
-    
-    for position in positions:
-        screen.blit(star,position)
 
 
-    screen.blit(meteor,meteor_rect)
-    screen.blit(laser,laser_rect)
-
-    #spaceship movement
-    spaceship_rect.x += direction * 0.2
-    if spaceship_rect.right > WINDOW_WIDTH or spaceship_rect.left < 0:
-        direction *= -1
-    screen.blit(spaceship,spaceship_rect)
-    
+    # if key[pygame.K_SPACE]:
+    #     print("laser")
+    # screen.blit(spaceship,spaceship_rect)
+    all_sprite.draw(screen)
     
     pygame.display.update()
 
