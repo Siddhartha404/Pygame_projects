6a1491f69bd4a1a5b595a60cc26148d223e5799d
Siddhartha404 <sthapa-chhetri2@myseneca.ca>
2025-01-05 03:06:54 -0700

    added basic lazer logic and metor logic

--- Space-Invaders/main.py
+++ Space-Invaders/main.py
@@ -1,6 +1,6 @@
 import pygame
 import pygame.display
-from random import randint
+from random import randint, uniform
 
 
 #initializing
@@ -20,6 +20,18 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT-50))
         self.direction = pygame.math.Vector2(1,1)
         self.speed = 300
+
+        #laser cooldown
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.cooldown = 300
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.cooldown:
+                self.can_shoot = True
+
     def update(self, dt):
         #spaceship movement
         key = pygame.key.get_pressed()
@@ -27,6 +39,16 @@ class Spaceship(pygame.sprite.Sprite):
         self.direction.y = int(key[pygame.K_DOWN]) - int(key[pygame.K_UP])
         self.direction = self.direction.normalize() if self.direction else self.direction
         self.rect.center += self.direction * self.speed * dt
+
+        #logic for lager
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE] and self.can_shoot:
+            laser_pos = self.rect.midtop
+            Laser(all_sprite, laser_img, laser_pos )
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+        self.laser_timer()
+
 
 class Star(pygame.sprite.Sprite):
     def __init__(self, groups, img):
@@ -38,31 +60,42 @@ class Meteor(pygame.sprite.Sprite):
     def __init__(self, groups, img):
         super().__init__(groups)   
         self.image = img
-        self.rect = self.image.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        self.rect = self.image.get_frect(center=((randint(0,WINDOW_WIDTH),randint(-200,-100))))
+        self.create_time = pygame.time.get_ticks()
+        self.speed = randint(400,500)
+        self.direction = pygame.Vector2(uniform(-0.5,0.5),1)
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        self.rect.center += self.direction * self.speed * dt
+        if current_time - self.create_time >= 3000:
+            self.kill()
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, groups, img):
+    def __init__(self, groups, img, pos):
         super().__init__(groups)   
         self.image = img
-        self.rect = self.image.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
-
+        self.rect = self.image.get_frect(midbottom = pos)
+        self.speed = 400
+    def update(self,dt):
+        self.rect.y -= self.speed * dt
+        if self.rect.bottom < 0:
+            self.kill()
 #all imports
 spaceship_img = pygame.image.load('images/spaceship.png').convert_alpha()
 star_img = pygame.image.load('images/star.png').convert_alpha()
 metero_img = pygame.image.load('images/meteor.png').convert_alpha()
 laser_img = pygame.image.load('images/laser.png').convert_alpha()
 
+#for sprites
 all_sprite = pygame.sprite.Group()
 for i in range(20):
     Star(all_sprite, star_img)
-meteor = Meteor(all_sprite, metero_img)
-laser = Laser(all_sprite, laser_img)
+
 spaceship = Spaceship(all_sprite, spaceship_img)
-
-
-
-
-
+ 
+#custom events
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 1000)
 
 #Main loop
 while running:
@@ -72,16 +105,15 @@ while running:
         #quitting the game
         if event.type == pygame.QUIT:
             running = False
-    
+        if event.type == meteor_event:
+            Meteor(all_sprite, metero_img)
     all_sprite.update(dt)
-
+    
     #drawing the screen
     screen.fill("darkgray")
 
-
     # if key[pygame.K_SPACE]:
     #     print("laser")
-    # screen.blit(spaceship,spaceship_rect)
     all_sprite.draw(screen)
     
     pygame.display.update()
